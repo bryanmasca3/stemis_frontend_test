e036f827d1b11076d714fb2729f9bdf5169dbaab
lan <bryanmasca3@gmail.com>
2023-07-11 15:17:22 -0500

    last changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/",
-  /*   baseURL: "https://stemis-backend.onrender.com/", */
+  /*   baseURL: "http://localhost:4000/", */
+  baseURL: "https://stemis-backend.onrender.com/",
 });
 
 export default instance;
